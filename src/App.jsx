160b3a94455743e9d73e70ee23c68a7b2c57ab4b
Nelsper <nelsper@gmail.com>
2023-09-15 08:11:41 -0500

    create navbar and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/navbar/NavBar";
 
+import Home from "./pages/home/Home";
 
 const App = () => {
 
   return(
     <>
-      
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path="/home" element={<Home/>}/>
+        </Routes>
+      </Router>
     </>
   )
 };
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./scss/styleHome.css";
+import fondo from "../../assets/images/fondo.jpg"
+
+
+const Home = () => {
+
+    return(
+        <>
+            <h1>Home</h1>
+            <img src={fondo} alt="" />
+        </>
+    )
+};
+
+
+export default Home;
